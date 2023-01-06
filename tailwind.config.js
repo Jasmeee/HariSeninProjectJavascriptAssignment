17c195119a617893033df30f15b245aa11d2a736
AsahRizky <ardiansyahrizky200@gmail.com>
2023-01-06 22:03:02 +0700

    init commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ales: "#F6F2E4",
+        alestext: "#6E7E2B",
+        alesph: "#7D794F",
+      },
+    },
   },
   plugins: [],
 };
